bdffc4bee34527b535838b3608c92f3d21a1a5c2
Zofia Janas <zofia.janas@mayeryn.com>
2020-07-01 17:59:42 +0200

    add indicator

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import { ThemeProvider } from 'styled-components';
 import ReactNotification from 'react-notifications-component';
 import 'react-notifications-component/dist/theme.css';
 
+import Indicator from './components/Shared/Indicator';
 import Header from './components/Header/Header';
-import Timeline from './components/Timeline/Timeline';
 import Footer from './components/Footer/Footer';
 
 import GlobalStyle from './styles/GlobalStyle';
 import { theme } from './styles/theme';
 
+const Timeline = React.lazy(() => import('./components/Timeline/Timeline'));
+
 const App = () => (
   <ThemeProvider theme={theme}>
     <GlobalStyle />
-    <ReactNotification />
-    <Header />
-    <Timeline />
-    <Footer />
+    <Suspense fallback={<Indicator />}>
+      <ReactNotification />
+      <Header />
+      <Timeline />
+      <Footer />
+    </Suspense>
   </ThemeProvider>
 );
 
